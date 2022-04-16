abce3336c27ac20c7f11b6c3cbb643cb00f465f2
Kannan Duraisamy <currentkannan@gmail.com>
2022-04-16 12:24:33 +0530

    Update ProductList.js

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,17 +1,53 @@
-import React from 'react'
-import {Link} from 'react-router-dom';
+import React from "react";
+import { View } from "./View";
+import { Link } from "react-router-dom";
 
-const ProductList = () => {
+export const ProductList = (props) => {
   return (
     <div>
-        <div className="subHeaderWrapper">
-        <h1 className="subHeader">Add item</h1>
-        <Link className="addItem" to="/add-item">
-          + Add item
+      <div className="itemWrapper">
+        <h1 className="itemHeader">Product List</h1>
+        <Link className="navButton add-item" to="/add-item">
+          + Add Item
         </Link>
       </div>
-    </div>
-  )
-}
 
-export default ProductList
+      <div className="view-container container">
+        {props.Items.length > 0 && (
+          <>
+            <table className="table">
+              <thead>
+                <tr>
+                  <th scope="col">Title</th>
+                  <th scope="col">Quantity</th>
+                  <th scope="col">Price</th>
+                  <th scope="col">Delete</th>
+                </tr>
+              </thead>
+              <tbody>
+                <View Items={props.Items} deleteItem={props.deleteItem} />
+              </tbody>
+            </table>
+            <div className="d-grid">
+              <button
+                className="btn btn-danger btn-lg"
+                onClick={() => props.setItems([])}
+              >
+                Remove All
+              </button>
+            </div>
+          </>
+        )}
+        {props.Items.length < 1 && (
+          <div className="emptyArray">
+            No Items to display...
+            <br />
+            Products added will be displayed here!!
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ProductList;
