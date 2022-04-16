155c7eaa50e27c48cba4584444bfe799eca814f2
Kannan Duraisamy <currentkannan@gmail.com>
2022-04-16 12:25:11 +0530

    Update View.js

--- src/components/View.js
+++ src/components/View.js
@@ -1,18 +1,22 @@
-import React from 'react'
-import {Icon} from 'react-icons-kit'
-import {trash} from 'react-icons-kit/feather/trash'
-export const View = ({Items,deleteItem}) => {
-    
-    return Items.map(Item=>(
-        
-        <tr key={Item.id}>
-            <td>{Item.title}</td>
-            <td>{Item.quantity}</td>
-            <td>{Item.price}</td>
-            <td className='delete-btn' onClick={()=>deleteItem(Item.id)}>
-                <Icon icon={trash}/>
-            </td>           
-        </tr>            
-    
-))
-}
+import React from "react";
+import { Icon } from "react-icons-kit";
+import { trash } from "react-icons-kit/feather/trash";
+
+export const View = ({ Items, deleteItem }) => {
+  function roundOff(x) {
+    return Number.parseFloat(x).toFixed(2);
+  }
+  return Items.map((Item) => (
+    <tr key={Item.id} scope="row">
+      <td>{Item.title}</td>
+      <td>{Item.quantity}</td>
+      <td className="roundOff">
+        {"₹ "}
+        {roundOff(Item.price)}
+      </td>
+      <td className="delete-btn" onClick={() => deleteItem(Item.id)}>
+        <Icon icon={trash} />
+      </td>
+    </tr>
+  ));
+};
