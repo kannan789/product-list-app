91ff5bb00528c7f25c1f9d1ce4a9111c71fdfafa
Kannan Duraisamy <currentkannan@gmail.com>
2022-04-16 12:23:28 +0530

    Update AddItem.js

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -1,139 +1,68 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import { View } from "./View";
 
-// getting the values of local storage
-const getDatafromLS = () => {
-  const data = localStorage.getItem("Items");
-  if (data) {
-    return JSON.parse(data);
-  } else {
-    return [];
-  }
-};
-
-export const AddItem = () => {
-  // item states
-  const [Items, setItems] = useState(getDatafromLS());
-
-  // field states
-  const [id, setId] = useState('');
-  const [title, setTitle] = useState('');
-  const [quantity, setQuantity] = useState('');
-  const [price, setPrice] = useState('');
-
-  // form submit event
-  const handleAddItemSubmit = (e) => {
-    e.preventDefault();
-    // creating an object
-    const randomNumber = parseInt(Math.random()*1000000)
-    let Item = {
-      id:randomNumber,
-      title,
-      quantity,
-      price,
-    };
-    setItems([...Items, Item]);
-    setId('');
-    setTitle('');
-    setQuantity('');
-    setPrice('');
-  };
-
-  // delete book from LS
-  const deleteItem = (id) => {
-    const filteredItems = Items.filter((element, index) => {
-      return element.id !== id;
-    })
-    setItems(filteredItems);
-  };
-
-  // const deleteItem = (index) => {
-  //   var newList = Items;
-  //   newList.splice(index,1)
-  //   setItems([...newList])
-  // }
-
-  // saving data to local storage
-  useEffect(() => {
-    localStorage.setItem('Items', JSON.stringify(Items));
-  }, [Items]);
-
+export const AddItem = (props) => {
   return (
     <div>
       <div className="itemWrapper">
-        {/* <Link className="goBack" to="/">
-          &lt; Back
-        </Link> */}
+        <Link className="products navButton" to="/">
+          &lt; Products
+        </Link>
+
         <h1 className="itemHeader">Add item</h1>
       </div>
 
-      <div className="form-container">
+      <div className="container form">
         <form
           autoComplete="off"
-          className="form-group"
-          onSubmit={handleAddItemSubmit}
+          className="form-group row g-3"
+          onSubmit={props.handleAddItemSubmit}
         >
-          <label>Title</label>
-          <input
-            type="text"
-            className="form-control"
-            required
-            onChange={(e) => setTitle(e.target.value)}
-            value={title}
-          ></input>
-          <br></br>
-          <label>Quantity</label>
-          <input
-            type="number"
-            className="form-control"
-            required
-            onChange={(e) => setQuantity(e.target.value)}
-            value={quantity}
-          ></input>
-          <br></br>
-          <label>Price</label>
-          <input
-            type="number"
-            className="form-control"
-            required
-            onChange={(e) => setPrice(e.target.value)}
-            value={price}
-          ></input>
-          <br></br>
-          <button type="submit" className="btn btn-success btn-md">
-            ADD
-          </button>
-        </form>
-      </div>
+          <div className="col-12">
+            <b>
+              <label>Title</label>
+            </b>
+            <input
+              type="text"
+              className="form-control"
+              required
+              onChange={(e) => props.setTitle(e.target.value)}
+              value={props.title}
+            ></input>
+          </div>
 
-      <div className="view-container">
-        {Items.length > 0 && 
-          <>
-            <div className="table-responsive">
-              <table className="table">
-                <thead>
-                  <tr>
-                    <th>Title</th>
-                    <th>Quantity</th>
-                    <th>Price</th>
-                    <th>Delete</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <View Items={Items} deleteItem={deleteItem} />
-                </tbody>
-              </table>
-            </div>
-            <button
-              className="btn btn-danger btn-md"
-              onClick={() => setItems([])}
-            >
-              Remove All
+          <div className="col-12">
+            <b>
+              <label>Quantity</label>
+            </b>
+            <input
+              type="text"
+              className="form-control mb-3"
+              required
+              onChange={(e) => props.setQuantity(e.target.value)}
+              value={props.quantity}
+            ></input>
+          </div>
+
+          <div className="col-12">
+            <b>
+              <label>Price</label>
+            </b>
+            <input
+              type="number"
+              className="form-control mb-3"
+              required
+              onChange={(e) => props.setPrice(e.target.value)}
+              value={props.price}
+            ></input>
+          </div>
+
+          <div className="d-grid">
+            <button type="submit" className="btn btn-lg">
+              ADD
             </button>
-          </>
-        }
-        {Items.length < 1 && <div>No Items are added yet</div>}
+          </div>
+        </form>
       </div>
     </div>
   );
